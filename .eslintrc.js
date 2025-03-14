41e1b08db8566141774a5b6e2f6ce3c83e1db2cb
IvanMonlau <ivantrubar@campus.monlau.com>
2025-03-14 16:11:35 +0100

    Documentación termianda

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser', // Define el parser que se utilizará para analizar el código TypeScript.
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+    project: 'tsconfig.json', // Especifica el archivo de configuración de TypeScript.
+    sourceType: 'module', // Define el tipo de módulo como 'module'.
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin'], // Especifica los plugins que se utilizarán, en este caso '@typescript-eslint/eslint-plugin'.
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended', // Extiende la configuración recomendada de '@typescript-eslint'.
+    'plugin:prettier/recommended', // Extiende la configuración recomendada de 'prettier'.
   ],
-  root: true,
+  root: true, // Indica que esta es la configuración raíz de ESLint.
   env: {
-    node: true,
-    jest: true,
+    node: true, // Define el entorno como Node.js.
+    jest: true, // Define el entorno como Jest para pruebas.
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js'], // Especifica los patrones de archivos que ESLint debe ignorar.
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off', // Desactiva la regla que requiere prefijos en los nombres de las interfaces.
+    '@typescript-eslint/explicit-function-return-type': 'off', // Desactiva la regla que requiere tipos de retorno explícitos en las funciones.
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Desactiva la regla que requiere tipos explícitos en los límites de los módulos.
+    '@typescript-eslint/no-explicit-any': 'off', // Desactiva la regla que prohíbe el uso del tipo 'any'.
   },
 };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,22 +3,22 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('AppController (e2e)', () => { // Describe un conjunto de pruebas end-to-end para el controlador 'AppController'. Utiliza la función 'describe' de Jest para agrupar pruebas.
+  let app: INestApplication; // delcaración de la variable 'app' de tipo 'INestApplication'. que se utilizará para almacenar la aplicación de NestJS.
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeEach(async () => { // Define una función asíncrona que se ejecuta antes de cada prueba.
+    const moduleFixture: TestingModule = await Test.createTestingModule({ // Crea un módulo de pruebas de NestJS.
+      imports: [AppModule], // Importa el módulo principal de la aplicación.
+    }).compile(); // Compila el módulo de pruebas.
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = moduleFixture.createNestApplication(); // Crea una instancia de la aplicación de NestJS. Para ello, obtiene la aplicación del módulo de pruebas.
+    await app.init(); // Inicializa la aplicación de NestJS.
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', () => { // Define una prueba que verifica que se pueda realizar una petición GET a la raíz de la aplicación.
+    return request(app.getHttpServer()) // Realiza una petición GET al servidor HTTP de la aplicación.
+      .get('/') // Realiza una petición GET a la raíz de la aplicación.
+      .expect(200) // Espera que la respuesta tenga un código de estado 200.
+      .expect('Hello World!'); // Espera que la respuesta sea 'Hello World!'.
   });
 });
